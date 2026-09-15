26099b338716638650ca39bb01b9104455184f15
ayoobjan98-lgtm <ayoobjan98@gmail.com>
2026-09-15 21:16:22 +0330

    update files

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit
+
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-gunicorn blogpost.wsgi:application
+python manage.py collectstatic --no-input
+
+python manage.py migrate
+
+gunicorn webblog.wsgi:application
