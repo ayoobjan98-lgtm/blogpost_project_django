2c0fcfd766c8174670733379fd214c924ddd1f9c
ayoobjan98-lgtm <ayoobjan98@gmail.com>
2026-09-17 01:55:58 +0330

    update settings

--- weblog/settings.py
+++ weblog/settings.py
@@ -109,14 +109,28 @@ WSGI_APPLICATION = "weblog.wsgi.application"
 # }
 
 
+
+
+import os
 import dj_database_url
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 # DATABASES = {
